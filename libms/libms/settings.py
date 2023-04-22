3f52e1cc4222d1e1b0f83d93f051ac3548bfc757
TQFonCoTR <90674917+TQFonCoTR@users.noreply.github.com>
2023-04-22 22:22:31 +0300

    railway

--- libms/libms/settings.py
+++ libms/libms/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+application = get_wsgi_application()
+app = application
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
